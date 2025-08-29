debug rotation artifacts (fix trajs during grouping + sliding window)

dataloader.py
@@ -40,7 +40,7 @@ class SliceDataset(Dataset):
         spf_aug=False,
         spokes_per_frame=None,
         weight_accelerations=False, 
-        initial_spokes_range=[2, 4, 8, 16, 24, 36]
+        initial_spokes_range=[8, 16, 24, 36]
     ):
         """
         Args:
@@ -215,6 +215,7 @@ class SliceDataset(Dataset):
             N_samples = kspace_slice.shape[-1]
             kspace = rearrange(kspace_slice, 't c sp sam -> t sp c sam')
             kspace_flat = kspace.contiguous().view(total_spokes, self.N_coils, N_samples)
+            # kspace_flat = kspace.contiguous().reshape(total_spokes, self.N_coils, N_samples)
 
             if self.spf_aug:
                 print("setting random spokes per frame...")
@@ -225,6 +226,7 @@ class SliceDataset(Dataset):
 
             N_time = total_spokes // spokes_per_frame
             kspace_binned = kspace_flat.view(N_time, spokes_per_frame, self.N_coils, N_samples)
+            # kspace_binned = kspace_flat.reshape(N_time, spokes_per_frame, self.N_coils, N_samples)
             kspace_slice = rearrange(kspace_binned, 't sp c sam -> t c sp sam')
         else:
             N_time = self.N_time
@@ -460,6 +462,10 @@ class SimulatedDataset(Dataset):
         # CSMaps: (H, W, C) -> (1, C, H, W) [batch, coils, h, w]
         csmaps_torch = torch.from_numpy(csmaps).permute(2, 0, 1).unsqueeze(0)
 
+        # csmaps_torch = torch.rot90(csmaps_torch, k=2, dims=[-2, -1])
+        # ground_truth_torch = torch.rot90(ground_truth_torch, k=2, dims=[-2, -1])
+        # grasp_recon_torch = torch.rot90(grasp_recon_torch, k=2, dims=[-3, -1])
+
         return kspace_torch, csmaps_torch, ground_truth_torch, grasp_recon_torch, mask, grasp_path #, parMap, aif, S0, T10, mask
     
 
@@ -577,6 +583,10 @@ class SimulatedSPFDataset(Dataset):
         # Ground truth: (H, W, T) -> (2, T, H, W) [real/imag, time, h, w]
         ground_truth_torch = torch.from_numpy(ground_truth_complex).permute(2, 0, 1) # T, H, W
         ground_truth_torch = torch.stack([ground_truth_torch.real, ground_truth_torch.imag], dim=0)
+
+        # smap_torch = torch.rot90(smap_torch, k=2, dims=[-2, -1])
+        # simImg_torch = torch.rot90(simImg_torch, k=2, dims=[0, 1])
+        # grasp_recon_torch = torch.rot90(grasp_recon_torch, k=2, dims=[-3, -1])
 
 
         return smap_torch, simImg_torch, grasp_recon_torch, mask, grasp_path #, parMap, aif, S0, T10, mask

train.py
@@ -142,11 +142,8 @@ else:
 curriculum_enabled = config['training']['curriculum_learning']['enabled']
 curriculum_phases = config['training']['curriculum_learning']['phases']
 
-print(curriculum_enabled)
-print(curriculum_phases)
-
 # Initial setup for train_dataset based on the first phase if curriculum is enabled
-initial_train_spokes_range = [2, 4, 8, 16, 24, 36]
+initial_train_spokes_range = [8, 16, 24, 36]
 if curriculum_enabled:
     if not curriculum_phases:
         raise ValueError("Curriculum learning enabled but no phases defined in config.yaml")
@@ -413,30 +410,28 @@ if args.from_checkpoint == False and config['debugging']['calc_step_0'] == True:
             # prepare inputs
             measured_kspace = to_torch_complex(measured_kspace).squeeze()
             measured_kspace = rearrange(measured_kspace, 't co sp sam -> co (sp sam) t')
+
+            # prep physics operators
+            ktraj, dcomp, nufft_ob, adjnufft_ob = prep_nufft(N_samples, N_spokes, N_time)
+            ktraj = ktraj.to(device)
+            dcomp = dcomp.to(device)
+            nufft_ob = nufft_ob.to(device)
+            adjnufft_ob = adjnufft_ob.to(device)
+
             
             if N_time > Ng:
-                # prep physics operators
-                ktraj, dcomp, nufft_ob, adjnufft_ob = prep_nufft(N_samples, N_spokes, Ng)
-                ktraj = ktraj.to(device)
-                dcomp = dcomp.to(device)
-                nufft_ob = nufft_ob.to(device)
-                adjnufft_ob = adjnufft_ob.to(device)
-
-                physics = MCNUFFT(nufft_ob, adjnufft_ob, ktraj, dcomp)
-
                 max_idx = N_time - Ng
                 random_index = random.randint(0, max_idx - 1) 
 
                 measured_kspace = measured_kspace[..., random_index:random_index + Ng]
 
-            else:
-                # prep physics operators
-                ktraj, dcomp, nufft_ob, adjnufft_ob = prep_nufft(N_samples, N_spokes, N_time)
-                ktraj = ktraj.to(device)
-                dcomp = dcomp.to(device)
-                nufft_ob = nufft_ob.to(device)
-                adjnufft_ob = adjnufft_ob.to(device)
+                ktraj_chunk = ktraj[..., random_index:random_index + Ng]
+                dcomp_chunk = dcomp[..., random_index:random_index + Ng]
 
+
+                physics = MCNUFFT(nufft_ob, adjnufft_ob, ktraj_chunk, dcomp_chunk)
+
+            else:
                 physics = MCNUFFT(nufft_ob, adjnufft_ob, ktraj, dcomp)
 
 
@@ -530,7 +525,7 @@ if args.from_checkpoint == False and config['debugging']['calc_step_0'] == True:
 
             if N_time_eval > eval_chunk_size:
                 print("Performing sliding window eval...")
-                x_recon, adj_loss = sliding_window_inference(H, W, N_samples, int(N_spokes), N_time_eval, eval_chunk_size, eval_chunk_overlap, measured_kspace, csmap, acceleration_encoding, model, epoch="val0", device=device)  
+                x_recon, adj_loss = sliding_window_inference(H, W, N_time_eval, eval_ktraj, eval_dcomp, eval_nufft_ob, eval_adjnufft_ob, eval_chunk_size, eval_chunk_overlap, measured_kspace, csmap, acceleration_encoding, model, epoch="val0", device=device)  
             else:
                 x_recon, adj_loss, *_ = model(
                 measured_kspace.to(device), eval_physics, csmap, acceleration_encoding, epoch="val0", norm=config['model']['norm']
@@ -701,31 +696,26 @@ else:
             measured_kspace = to_torch_complex(measured_kspace).squeeze()
             measured_kspace = rearrange(measured_kspace, 't co sp sam -> co (sp sam) t')
 
+            # prep physics operators
+            ktraj, dcomp, nufft_ob, adjnufft_ob = prep_nufft(N_samples, N_spokes, N_time)
+            ktraj = ktraj.to(device)
+            dcomp = dcomp.to(device)
+            nufft_ob = nufft_ob.to(device)
+            adjnufft_ob = adjnufft_ob.to(device)
+
             if N_time > Ng:
-
-                # prep physics operators
-                ktraj, dcomp, nufft_ob, adjnufft_ob = prep_nufft(N_samples, N_spokes, Ng)
-                ktraj = ktraj.to(device)
-                dcomp = dcomp.to(device)
-                nufft_ob = nufft_ob.to(device)
-                adjnufft_ob = adjnufft_ob.to(device)
-
-                physics = MCNUFFT(nufft_ob, adjnufft_ob, ktraj, dcomp)
 
                 max_idx = N_time - Ng
                 random_index = random.randint(0, max_idx - 1) 
 
                 measured_kspace = measured_kspace[..., random_index:random_index + Ng]
+                ktraj_chunk = ktraj[..., random_index:random_index + Ng]
+                dcomp_chunk = dcomp[..., random_index:random_index + Ng]
+
+                physics = MCNUFFT(nufft_ob, adjnufft_ob, ktraj_chunk, dcomp_chunk)
                 
 
             else:
-                # prep physics operators
-                ktraj, dcomp, nufft_ob, adjnufft_ob = prep_nufft(N_samples, N_spokes, N_time)
-                ktraj = ktraj.to(device)
-                dcomp = dcomp.to(device)
-                nufft_ob = nufft_ob.to(device)
-                adjnufft_ob = adjnufft_ob.to(device)
-
                 physics = MCNUFFT(nufft_ob, adjnufft_ob, ktraj, dcomp)
 
 
@@ -883,13 +873,6 @@ else:
                     kspace_path = val_kspace_batch[0]
 
                     # SIMULATE KSPACE
-                    print("ground_truth_for_physics: ", ground_truth_for_physics.shape)
-                    print("val_csmap: ", val_csmap.shape)
-                    print("eval_physics.ktraj: ", eval_physics.ktraj.shape)
-                    print("spokes_per_frame: ", val_dro_dataset.spokes_per_frame)
-                    print("num_frames: ", val_dro_dataset.num_frames)
-                    print("N_time_eval: ", N_time_eval)
-                    print("N_spokes_eval: ", N_spokes_eval)
                     val_kspace_batch = eval_physics(False, ground_truth_for_physics, val_csmap)
 
                     # save k-space 
@@ -923,7 +906,7 @@ else:
                     
                 if N_time_eval > eval_chunk_size:
                     print("Performing sliding window eval...")
-                    val_x_recon, val_adj_loss = sliding_window_inference(H, W, N_samples, int(N_spokes), N_time_eval, eval_chunk_size, eval_chunk_overlap, val_kspace_batch, val_csmap, acceleration_encoding, model, epoch=f"val{epoch}", device=device)  
+                    val_x_recon, val_adj_loss = sliding_window_inference(H, W, N_time_eval, eval_ktraj, eval_dcomp, eval_nufft_ob, eval_adjnufft_ob, eval_chunk_size, eval_chunk_overlap, val_kspace_batch, val_csmap, acceleration_encoding, model, epoch=f"val{epoch}", device=device)  
                 else:
                     val_x_recon, val_adj_loss, *_ = model(
                     val_kspace_batch.to(device), eval_physics, val_csmap, acceleration_encoding, epoch=f"val{epoch}", norm=config['model']['norm']
@@ -1449,7 +1432,7 @@ with torch.no_grad():
 
             if num_frames > eval_chunk_size:
                 print("Performing sliding window eval...")
-                x_recon, _ = sliding_window_inference(H, W, N_samples, int(spokes), num_frames, eval_chunk_size, eval_chunk_overlap, kspace, csmap, acceleration_encoding, model, epoch=None, device=device)  
+                x_recon, _ = sliding_window_inference(H, W, num_frames, ktraj, dcomp, nufft_ob, adjnufft_ob, eval_chunk_size, eval_chunk_overlap, kspace, csmap, acceleration_encoding, model, epoch=None, device=device)  
             else:
                 x_recon, *_ = model(
                     kspace.to(device), physics, csmap, acceleration_encoding, epoch=None, norm=config['model']['norm']
@@ -1592,7 +1575,7 @@ with torch.no_grad():
 
             if num_frames > eval_chunk_size:
                 print("Performing sliding window eval...")
-                x_recon, _ = sliding_window_inference(H, W, N_samples, int(spokes), num_frames, eval_chunk_size, eval_chunk_overlap, kspace, csmap, acceleration_encoding, model, epoch=None, device=device)  
+                x_recon, _ = sliding_window_inference(H, W, num_frames, ktraj, dcomp, nufft_ob, adjnufft_ob, eval_chunk_size, eval_chunk_overlap, kspace, csmap, acceleration_encoding, model, epoch=None, device=device)  
             else:
                 x_recon, *_ = model(
                 kspace.to(device), physics, csmap, acceleration_encoding, epoch=None, norm=config['model']['norm']

utils.py
@@ -566,13 +566,13 @@ def generate_sliding_window_indices(N_frames, chunk_size, overlap_size):
 
 
 
-def sliding_window_inference(H, W, N_samples, N_spokes, N_frames, chunk_size, chunk_overlap, kspace, csmap, acceleration_encoding, model, epoch, device):
+def sliding_window_inference(H, W, N_frames, ktraj, dcomp, nufft_ob, adjnufft_ob, chunk_size, chunk_overlap, kspace, csmap, acceleration_encoding, model, epoch, device):
 
     chunk_indices = generate_sliding_window_indices(N_frames, chunk_size, chunk_overlap)
 
     # define physics object for chunk of timeframes
-    ktraj_chunk, dcomp_chunk, nufft_ob_chunk, adjnufft_ob_chunk = prep_nufft(N_samples, N_spokes, chunk_size)
-    physics_chunk = MCNUFFT(nufft_ob_chunk.to(device), adjnufft_ob_chunk.to(device), ktraj_chunk.to(device), dcomp_chunk.to(device))
+    # ktraj_chunk, dcomp_chunk, nufft_ob_chunk, adjnufft_ob_chunk = prep_nufft(N_samples, N_spokes, chunk_size)
+    # physics_chunk = MCNUFFT(nufft_ob_chunk.to(device), adjnufft_ob_chunk.to(device), ktraj_chunk.to(device), dcomp_chunk.to(device))
 
     # Define tensor for stitched reconstruction
     stitched_recon = torch.zeros(1, 2, H, W, N_frames).to(device)
@@ -587,6 +587,11 @@ def sliding_window_inference(H, W, N_samples, N_spokes, N_frames, chunk_size, ch
 
         # select chunk from k-space
         kspace_chunk = kspace[..., start_idx:end_idx]
+        ktraj_chunk = ktraj[..., start_idx:end_idx]
+        dcomp_chunk = dcomp[..., start_idx:end_idx]
+
+        physics_chunk = MCNUFFT(nufft_ob, adjnufft_ob, ktraj_chunk, dcomp_chunk)
+
 
         # generate reconstruction
         x_recon_chunk, adj_loss, *_ = model(